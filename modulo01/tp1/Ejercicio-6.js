4042ce2c805b79064310b6c645194d33040fdc68
Jorge Eduardo Ferreira <113069257+JorgeEduardoFerreira@users.noreply.github.com>
2022-10-03 23:45:29 -0300

    Create Ejercicio-6.js

---
+++ modulo01/tp1/Ejercicio-6.js
@@ -0,0 +1,9 @@
+/*Ejercicio 6:
+Crear un array llamado “dias” y que almacene el nombre de los siete días de la semana.
+Mostrar por pantalla los siete nombres utilizando la función console.log().
+*/
+
+// se declara el array y se cargan los elementos
+var1 = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes"]
+//se imprime en los elementos en la consola
+console.log (var1)
