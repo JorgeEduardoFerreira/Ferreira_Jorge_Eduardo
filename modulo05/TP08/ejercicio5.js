0bf23bd1fb61b1f00e72097d21748245af492032
Jorge Eduardo Ferreira <113069257+JorgeEduardoFerreira@users.noreply.github.com>
2022-11-22 01:56:56 -0300

    Add files via upload
    Se pudo hacer funcionar, no como se queria pero funciona

--- modulo05/TP08/ejercicio5.js
+++ modulo05/TP08/ejercicio5.js
@@ -11,22 +11,30 @@ Pedro Mendez FullStack js
 const fs = require('fs');
 const rawdata = fs.readFileSync('./departamentos.json', 'utf8');
 var departa = JSON.parse(rawdata);
-console.log (departa)
+// console.log (departa)
 
 getNombreyPuesto (departa);
 
-console.log("----------");
-console.log(departa['departamentos'].length);
-console.log("-----***-----");
-console.log(departa[{['departamentos']:['Tecnología']}].length);
-
+// console.log("----------");
+// console.log(departa['departamentos'].length);
+// console.log("-----***-----");
+// console.log(departa['departamentos'][1].length);
+// console.log(departa['departamentos'][1]['Tecnologia'].length);
 
 function getNombreyPuesto (Array) { 
     for(var i=0;i<Array['departamentos'].length ; i++){
-        console.log(i);
-        console.log(Array['departamentos'][i]);
-        console.log(Array['departamentos'][i]).length; ////////////NO FUNCIONA////////////////
-               
-        //console.log(Array['departamentos'],['Tecnología'].length); 
+        //  console.log(Array['departamentos'].length);
+        //  console.log(i);
+        //  console.log(Array['departamentos'][i]);
+        if (i==1) {       // EN VEZ DE 1 QUIERO COMPARAR SI DICE TECNOLOGIA  
+            console.log(i);
+            for(var j=0;j<Array['departamentos'][i]['Tecnologia'].length ; j++){
+                //  console.log(j);
+                //  console.log(Array['departamentos'][i]);
+                console.log(Array['departamentos'][1]['Tecnologia'][j]['nombre'] + ' ' + Array['departamentos'][1]['Tecnologia'][j]['puesto'])
+                };
+                return;
+            
+        }
     };
 }; 
